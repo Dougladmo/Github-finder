374c12c7085684924180701e24992f7d303e5173
dougladmo <Douglas.costa7417@gmail.com>
2023-12-26 07:25:20 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Pages
+import Home from './routes/Home.jsx'
+import Repos from './routes/Repos.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/repos/:username",
+        element: <Repos />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
